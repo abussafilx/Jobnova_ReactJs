d5f3c2d76e6018e8ff6a1d0e3f91e93a3a8bf3c3
Victor Abussafi <victorabussafi@gmail.com>
2025-04-21 21:50:46 +0100

    netlify fix 2

--- src/pages/comingsoon.js
+++ src/pages/comingsoon.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react' 
+import React, { useEffect, useState, useCallback } from 'react' 
 import { Link } from "react-router-dom";
 import bg1 from "../assets/images/hero/bg2.jpg"
 import logo from "../assets/images/logo-icon-80-white.png"
