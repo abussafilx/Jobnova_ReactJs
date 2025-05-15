9f0edf0f49a0a2304d8c074e9ca988ac84f0b742
Victor Abussafi <victorabussafi@gmail.com>
2025-05-15 20:42:54 +0100

    netlifiy fix 2

--- src/componants/categories.js
+++ src/componants/categories.js
@@ -7,38 +7,38 @@ import { jobData } from "../data/data";
 import { FiClock, FiDollarSign, FiMapPin } from "../assets/icons/vander";
 
 export default function Categories(){
-    let settings = {
-        container: '.tiny-five-item',
-        controls: true,
-        mouseDrag: true,
-        loop: true,
-        rewind: true,
-        autoplay: true,
-        autoplayButtonOutput: false,
-        autoplayTimeout: 3000,
-        navPosition: "bottom",
-        controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
-        nav: false,
-        speed: 400,
-        gutter: 0,
-        responsive: {
-            1025: {
-                items: 5
-            },
+    // let settings = {
+    //     container: '.tiny-five-item',
+    //     controls: true,
+    //     mouseDrag: true,
+    //     loop: true,
+    //     rewind: true,
+    //     autoplay: true,
+    //     autoplayButtonOutput: false,
+    //     autoplayTimeout: 3000,
+    //     navPosition: "bottom",
+    //     controlsText: ['<i class="mdi mdi-chevron-left "></i>', '<i class="mdi mdi-chevron-right"></i>'],
+    //     nav: false,
+    //     speed: 400,
+    //     gutter: 0,
+    //     responsive: {
+    //         1025: {
+    //             items: 5
+    //         },
 
-            992: {
-                items: 4
-            },
+    //         992: {
+    //             items: 4
+    //         },
 
-            767: {
-                items: 3
-            },
+    //         767: {
+    //             items: 3
+    //         },
 
-            425: {
-                items: 1
-            },
-        },
-      };
+    //         425: {
+    //             items: 1
+    //         },
+    //     },
+    //   };
     return(
         <>
             <div className="container mt-100 mt-60">
